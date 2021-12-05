734b960c2ec2de4bbaa64c77093571e8a62e2890
duktartamang <duktar13@gmail.com>
2021-12-05 22:32:23 +0545

    Quesntion3

---
+++ src/Assignment1/Sorting.java
@@ -0,0 +1,38 @@
+package Assignment1;
+
+import java.util.*;
+
+class Sorting {
+        void SortStrings() {
+        Scanner s = new Scanner(System.in);
+        System.out.print("enter a number of strings n:");
+        int n = s.nextInt();
+        String[] str = new String[n];
+        System.out.println();
+        System.out.print("enter the strings");
+        for(int i =0;i<n;i++){
+            str[i] = new String(s.next());
+        }
+        for(int i =0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(str[i].compareTo(str[j])>0){
+                    String temp = str[i];
+                    str[i] = str[j];
+                    str[j] = temp;
+
+                }
+            }
+        }
+        System.out.println("sorted string");
+        for(int i=0;i<n;i++){
+            System.out.println(str[i]);
+        }
+    }
+}
+class driver
+{
+    public static void main(String[] args) {
+        Sorting obj = new Sorting();
+        obj.SortStrings();
+    }
+}
